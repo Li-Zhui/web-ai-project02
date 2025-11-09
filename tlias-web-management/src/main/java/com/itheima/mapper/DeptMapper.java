f9aea3ccf9e8a0a452c9eea155403f4099f5bfd8
Li Zhui <3305146046@qq.com>
2025-11-09 12:26:43 +0800

    78.部门管理-列表查询-结果封装

--- tlias-web-management/src/main/java/com/itheima/mapper/DeptMapper.java
+++ tlias-web-management/src/main/java/com/itheima/mapper/DeptMapper.java
@@ -2,6 +2,8 @@ package com.itheima.mapper;
 
 import com.itheima.pojo.Dept;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -11,6 +13,16 @@ public interface DeptMapper {
     /**
      * 查询所有部门数据
      */
+
+    //方式一：手动结果映射
+//    @Results({
+//            @Result(column = "create_time",property = "createTime"),
+//            @Result(column = "update_time",property = "updateTime")
+//    })
+
+    //方式二：起别名
+    //@Select("select id, name, create_time createTime, update_time updateTime from dept order by update_time desc")
+
     @Select("select id, name, create_time, update_time from dept order by update_time desc")
     List<Dept> findAll();
 }
