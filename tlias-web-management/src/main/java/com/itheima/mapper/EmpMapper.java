cb1cb36ab5d11150f6c36c834d80ab96cde1da00
Li Zhui <3305146046@qq.com>
2025-11-17 12:56:02 +0800

    93.员工管理-准备工作

---
+++ tlias-web-management/src/main/java/com/itheima/mapper/EmpMapper.java
@@ -0,0 +1,10 @@
+package com.itheima.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 操作员工信息
+ */
+@Mapper
+public interface EmpMapper {
+}
---
+++ tlias-web-management/src/main/java/com/itheima/mapper/EmperMapper.java
@@ -0,0 +1,10 @@
+package com.itheima.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 操作员工工作经历信息
+ */
+@Mapper
+public interface EmperMapper {
+}
